df95f6e9a647463ec7f4beaf260e143ddbb7f85c
Dmitry Bublik <dmitry.buba@gmail.com>
2016-04-11 01:27:42 +0300

    Added Windows Service project.

---
+++ TouchInjection.WindowsService/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace TouchInjection.WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new TouchInjectionService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ TouchInjection.WindowsService/TouchInjectionService.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceProcess;
+
+namespace TouchInjection.WindowsService
+{
+    public partial class TouchInjectionService : ServiceBase
+    {
+        //private readonly ITouchInjectionListener _listener;
+
+        public TouchInjectionService()
+        {
+            InitializeComponent();
+
+            //var executor = new TouchInjectionExecutor();
+            //var hook = new UserActivityHook();
+
+            //_listener = new TouchInjectionListener(
+            //    new TouchInjectionProvider(
+            //        new MouseLocationProvider(hook),
+            //        new KeyboardActionProvider(hook),
+            //        hook),
+            //    executor);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //_listener.Start();
+        }
+
+        protected override void OnStop()
+        {
+            //_listener.Stop();
+            //Application.Current.Shutdown();
+        }
+    }
+}
