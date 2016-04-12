9d8755e73d09c376fcafd4d4d5997d888ce8395d
Dmitry Bublik <dmitry.buba@gmail.com>
2016-04-13 00:07:13 +0300

    Added missing files.

---
+++ TouchInjection.Services/MouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using JetBrains.Annotations;
+
+namespace TouchInjection.Services
+{
+    [UsedImplicitly]
+    public sealed class MouseController : IMouseController
+    {
+        public Point Location
+        {
+            get { return MouseSimulator.Position; }
+            set { MouseSimulator.Position = value; }
+        }
+        public void Wheel(int delta)
+        {
+            MouseSimulator.MouseWheel(delta);
+        }
+
+        public void HorizontalWheel(int delta)
+        {
+            MouseSimulator.MouseHWheel(delta);
+        }
+    }
+}
